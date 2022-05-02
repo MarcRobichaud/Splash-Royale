72799af65744e3914e7e9cdb02d7b9e20e2f906e
MarcRobichaud <marc_robi@yahoo.ca>
2022-05-02 04:35:41 -0400

    Spawning On click

--- Assets/Scripts/Networking/Player.cs
+++ Assets/Scripts/Networking/Player.cs
@@ -1,11 +1,15 @@
 using Unity.Netcode;
+using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    private GameObject go;
+    
     private void Start()
     {
         if (IsOwner)
         {
+            go = Resources.Load<GameObject>("prefabs/cube");
             InitPlayerStatsServerRpc(OwnerClientId, IsHost);
         }
     }
@@ -16,7 +20,25 @@ public class Player : NetworkBehaviour
         GameManager.Instance.Init(id, isHost);
     }
 
-    private void Spawn()
+    private void Update()
     {
+        if (IsOwner && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                SpawnServerRpc(OwnerClientId, hit.transform.position);
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void SpawnServerRpc(ulong id, Vector3 position)
+    {
+        GameObject nobj = Instantiate(go, position, Quaternion.identity);
+        nobj.GetComponent<NetworkObject>().Spawn();
+        Unit unit = nobj.GetComponent<Unit>();
+        unit.Init(id);
     }
 }
